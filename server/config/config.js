6226899159fba63c831ee8728108de21dff02ee8
Dalvinder <ss_vinder@hotmail.com>
2020-01-29 21:05:08 -0400

    Variable de Entorno

--- server/config/config.js
+++ server/config/config.js
@@ -15,7 +15,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://dal_sali_sald:NVOG5iqCv9qJb8zh@cluster0-w6qd1.mongodb.net/test';
+    urlDB = process.env.MONGO_URL;
 }
 
 process.env.URLDB = urlDB;
